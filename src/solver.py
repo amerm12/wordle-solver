3b55f92183c200922db785f3afcf61dcfe24e8bf
amerm12 <amer.mulaosmanovic1@gmail.com>
2025-11-04 00:41:30 +0100

    Continue implementing logic for misplaced letters. Still does not work on some occasions.

--- src/solver.py
+++ src/solver.py
@@ -8,62 +8,87 @@ class WordleSolver:
     def __init__(self):
         self.wrongLetters = []
         self.correctLetters = [(), (), (), (), ()]
-        self.misplacedLetters = []
+        self.misplacedLetters = [[], [], [], [], []]
 
     def analyzeImage(self):
+        print("test")
         pass
 
     # C - Correct
     # I - Incorrect
     # M - Misplaced, wrong spot
     def suggestWords(self, _words):
-        # ToDo: Handle logic where words have double letters
         if len(_words) <= 0:
             print("There are no words")
             return
 
         words = _words
         for index, (letter, key) in enumerate(words):
-            if index >= 5:
-                index = index - 5
-
+            # Keep index from 0 to 4
+            index = index % 5
             match key:
                 case "I":
                     self.wrongLetters.append(letter.lower())
                 case "C":
                     self.correctLetters[index] = letter.lower()
                 case "M":
-                    if self.misplacedLetters == []:
-                        self.misplacedLetters.append((letter.lower(), [index]))
-                    else:
-                        i = 0
-                        while i < len(self.misplacedLetters):
-                            if (
-                                self.misplacedLetters[i][0] == letter.lower()
-                                and index not in self.misplacedLetters[i][1]
-                            ):
-                                self.misplacedLetters[i][1].append(index)
-
-                            i += 1
+                    if letter.lower() not in self.misplacedLetters[index]:
+                        self.misplacedLetters[index].append(letter.lower())
 
         with open(
             "C:/Users/amera/OneDrive/Desktop/Skafiskafnjak/Amer/wordle-solver/solutions.txt"
         ) as f:
-            words = f.read().splitlines()
+            fileWords = f.read().splitlines()
 
-        # First check if there are some letters in both groups. If there are new letter to
-        # correct letters group, remove them from misplaced letters group.
+        # Remove all correct letters from misplaced letters group
         wrongLettersString = "".join(self.wrongLetters)
+        """ for correctLetter in self.correctLetters:
+            for misplacedPosition in self.misplacedLetters:
+                if correctLetter in misplacedPosition:
+                    misplacedPosition.remove(correctLetter) """
 
+        # Construct regex dynamically
+        # ToDo: Make a new function and move the code
         regex = ""
         for x in range(5):
-            if self.correctLetters[x] == ():
-                regex = regex + f"[^{re.escape(wrongLettersString)}]"
-            else:
+            # Letter in a position is correct, just set it
+            if self.correctLetters[x] != ():
                 regex = regex + self.correctLetters[x]
+            # There is no correct letter in a position, create regex
+            elif self.correctLetters[x] == ():
+
+                misplacedLettersString = ""
+                onPositionWrongString = ""
+                for position in self.misplacedLetters:
+                    for character in position:
+                        if (
+                            character not in misplacedLettersString
+                            and character not in self.misplacedLetters[x]
+                        ):
+                            misplacedLettersString = misplacedLettersString + character
+                        elif (
+                            character in self.misplacedLetters[x]
+                            and character not in wrongLettersString
+                        ):
+                            # onPositionWrongString = onPositionWrongString + character
+                            onPositionWrongString = onPositionWrongString + character
+
+                regex = (
+                    regex
+                    + f"([{re.escape(misplacedLettersString)}]|[^{re.escape(wrongLettersString + onPositionWrongString)}])"
+                )
+
+        # print(regex)
 
         pattern = re.compile(regex)
 
-        matches = list(filter(pattern.findall, words))
+        matches = list(filter(pattern.findall, fileWords))
 
         print(matches)
+
+
+## If if there is no correct letter for that position, rules:
+# - On that position it should be a letter from misplaced letter, from which are selected
+#   all except ones on that position.
+# - On that position there shouldn't be any letters from wrong letters.
+# - On that position there shouldn't be any letters that are misplaced on that position.
--- src/test.py
+++ src/test.py
@@ -4,17 +4,4 @@ solver = WordleSolver()
 
 # word = [("P", "I"), ("L", "M"), ("A", "C"), ("N", "I"), ("T", "I")]
 
-words = [
-    ("R", "M"),  # RAVES ABORT
-    ("A", "M"),
-    ("V", "I"),
-    ("E", "I"),
-    ("S", "I"),
-    ("A", "C"),
-    ("B", "C"),
-    ("O", "M"),
-    ("R", "M"),
-    ("T", "I"),
-]
-
-solver.suggestWords(words)
+solver.analyzeImage()
