7253722693b5d0162ecb5cc8c4f345b2ab574f92
amerm12 <amer.mulaosmanovic1@gmail.com>
2025-12-05 18:09:52 +0100

    Change test that currently acts as a main so it only calls gui

--- src/test.py
+++ src/test.py
@@ -1,10 +1,3 @@
 from gui import SolverGui
-from solver import WordleSolver
 
-words = [("P", "C"), ("E", "M"), ("T", "I"), ("A", "C"), ("L", "M")]
-
-solver = WordleSolver()
-suggestedWords = solver.suggestWords(words)
-print(suggestedWords)
-
-# gui = SolverGui()
+gui = SolverGui()
