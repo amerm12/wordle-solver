cfd086e03c0f7e2902c63a4c0c3deaa52480f4bc
amerm12 <amer.mulaosmanovic1@gmail.com>
2025-12-05 18:05:59 +0100

    Add images of all letters for template matching, and use dictionary to store letters as keys, and paths as values.

---
+++ src/templateMap.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+assetsPath = Path(__file__).resolve().parent.parent / "assets"
+
+TEMPLATES = {
+    "A": assetsPath / "letterA.png",  # A
+    "B": assetsPath / "letterB.png",  # B
+    "C": assetsPath / "letterC.png",  # C
+    "D": assetsPath / "letterD.png",  # D
+    "E": assetsPath / "letterE.png",  # E
+    "F": assetsPath / "letterF.png",  # F
+    "G": assetsPath / "letterG.png",  # G
+    "H": assetsPath / "letterH.png",  # H
+    "I": assetsPath / "letterI.png",  # I
+    "J": assetsPath / "letterJ.png",  # J
+    "K": assetsPath / "letterK.png",  # K
+    "L": assetsPath / "letterL.png",  # L
+    "M": assetsPath / "letterM.png",  # M
+    "N": assetsPath / "letterN.png",  # N
+    "O": assetsPath / "letterO.png",  # O
+    "P": assetsPath / "letterP.png",  # P
+    "Q": assetsPath / "letterQ.png",  # Q
+    "R": assetsPath / "letterR.png",  # R
+    "S": assetsPath / "letterS.png",  # S
+    "T": assetsPath / "letterT.png",  # T
+    "U": assetsPath / "letterU.png",  # U
+    "V": assetsPath / "letterV.png",  # V
+    "W": assetsPath / "letterW.png",  # W
+    "X": assetsPath / "letterX.png",  # X
+    "Y": assetsPath / "letterY.png",  # Y
+    "Z": assetsPath / "letterZ.png",  # Z
+}
