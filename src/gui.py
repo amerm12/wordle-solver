f50d4b0d89a79d1a9fd626452c7ca7774b17848a
amerm12 <amer.mulaosmanovic1@gmail.com>
2025-12-05 18:07:55 +0100

    Add logo and some more visual improvements

--- src/gui.py
+++ src/gui.py
@@ -1,27 +1,50 @@
 import customtkinter
 from analyzer import ImageAnalyzer
 from solver import WordleSolver
+from PIL import Image, ImageTk
 
 
 class SolverGui:
 
     # Initializes gui, sets default settings
     def __init__(self):
-        self.app = customtkinter.CTk()
+        self.app = customtkinter.CTk(fg_color="#121213")
         self.app.title("wordle-solver")
-        self.app.geometry("400x400")
+        self.app.geometry("500x600")
         self.app.resizable(False, False)
         self.app.grid_columnconfigure(0, weight=1)
 
+        # Define label for logo
+        self.logoLabel = customtkinter.CTkLabel(
+            self.app,
+            font=("Helvetica Neue", 40, "bold"),
+            text="wordle-solver",
+            text_color="#538d4e",
+        )
+        self.logoLabel.grid(row=0, column=0, pady=30)
+
         # Define upload button
         self.button = customtkinter.CTkButton(
-            self.app, text="Upload", command=self.uploadImage
+            self.app,
+            text="Upload",
+            font=("Helvetica", 24, "bold"),
+            width=200,
+            height=60,
+            fg_color="#74a67a",
+            hover_color="#5f8c5c",
+            text_color="#ffffff",
+            command=self.uploadImage,
         )
-        self.button.grid(row=0, column=0, padx=20, pady=20)
+        self.button.grid(row=1, column=0, pady=10)
 
-        # Define empty label
-        self.label = customtkinter.CTkLabel(self.app, text="")
-        self.label.grid(row=1, column=0, padx=20, pady=20)
+        # Define empty label for recommended words
+        self.label = customtkinter.CTkLabel(
+            self.app,
+            font=("Helvetica", 24),
+            text="",
+            text_color="#ffffff",
+        )
+        self.label.grid(row=2, column=0, padx=20, pady=20)
 
         self.app.mainloop()
 
@@ -36,6 +59,11 @@ class SolverGui:
             ),
         )
 
+        # If user closes file dialog
+        if not filepath:
+            return
+
+        # Do analyzing and solving logic
         analyzer = ImageAnalyzer()
         solver = WordleSolver()
         letters = analyzer.analyzeImage(filepath)
@@ -46,6 +74,7 @@ class SolverGui:
 
     # Display suggested words in the empty label and update button text
     def displayWords(self, words):
+        words = "\n".join(words)
         self.button.configure(text="Upload New")
 
         self.label.configure(text=words)
