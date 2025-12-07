5efbc5d32bc84f7988e5cce8a422561d27a9d0f1
amerm12 <amer.mulaosmanovic1@gmail.com>
2025-12-07 20:30:06 +0100

    Remove redundant comments

--- src/templateMap.py
+++ src/templateMap.py
@@ -1,32 +1,33 @@
 from pathlib import Path
 
+# Path to assets folder. Paths to each letter are just added to this base path.
 assetsPath = Path(__file__).resolve().parent.parent / "assets"
 
 TEMPLATES = {
-    "A": assetsPath / "letterA.png",  # A
-    "B": assetsPath / "letterB.png",  # B
-    "C": assetsPath / "letterC.png",  # C
-    "D": assetsPath / "letterD.png",  # D
-    "E": assetsPath / "letterE.png",  # E
-    "F": assetsPath / "letterF.png",  # F
-    "G": assetsPath / "letterG.png",  # G
-    "H": assetsPath / "letterH.png",  # H
-    "I": assetsPath / "letterI.png",  # I
-    "J": assetsPath / "letterJ.png",  # J
-    "K": assetsPath / "letterK.png",  # K
-    "L": assetsPath / "letterL.png",  # L
-    "M": assetsPath / "letterM.png",  # M
-    "N": assetsPath / "letterN.png",  # N
-    "O": assetsPath / "letterO.png",  # O
-    "P": assetsPath / "letterP.png",  # P
-    "Q": assetsPath / "letterQ.png",  # Q
-    "R": assetsPath / "letterR.png",  # R
-    "S": assetsPath / "letterS.png",  # S
-    "T": assetsPath / "letterT.png",  # T
-    "U": assetsPath / "letterU.png",  # U
-    "V": assetsPath / "letterV.png",  # V
-    "W": assetsPath / "letterW.png",  # W
-    "X": assetsPath / "letterX.png",  # X
-    "Y": assetsPath / "letterY.png",  # Y
-    "Z": assetsPath / "letterZ.png",  # Z
+    "A": assetsPath / "letterA.png",
+    "B": assetsPath / "letterB.png",
+    "C": assetsPath / "letterC.png",
+    "D": assetsPath / "letterD.png",
+    "E": assetsPath / "letterE.png",
+    "F": assetsPath / "letterF.png",
+    "G": assetsPath / "letterG.png",
+    "H": assetsPath / "letterH.png",
+    "I": assetsPath / "letterI.png",
+    "J": assetsPath / "letterJ.png",
+    "K": assetsPath / "letterK.png",
+    "L": assetsPath / "letterL.png",
+    "M": assetsPath / "letterM.png",
+    "N": assetsPath / "letterN.png",
+    "O": assetsPath / "letterO.png",
+    "P": assetsPath / "letterP.png",
+    "Q": assetsPath / "letterQ.png",
+    "R": assetsPath / "letterR.png",
+    "S": assetsPath / "letterS.png",
+    "T": assetsPath / "letterT.png",
+    "U": assetsPath / "letterU.png",
+    "V": assetsPath / "letterV.png",
+    "W": assetsPath / "letterW.png",
+    "X": assetsPath / "letterX.png",
+    "Y": assetsPath / "letterY.png",
+    "Z": assetsPath / "letterZ.png",
 }
