ac411f3071cdc369bca1be4080c1d85e0deac393
amerm12 <amer.mulaosmanovic1@gmail.com>
2025-12-13 02:46:48 +0100

    Implement screen selector class

---
+++ src/screenSelector.py
@@ -0,0 +1,56 @@
+import customtkinter
+from PIL import ImageGrab
+
+
+class ScreenSelector(customtkinter.CTkToplevel):
+    def __init__(self, parent, callback):
+        super().__init__(parent)
+
+        self.callback = callback
+
+        self.attributes("-fullscreen", True)
+        self.attributes("-alpha", 0.3)
+        self.configure(bg="black")
+        self.bind("<Escape>", lambda e: self.destroy())
+
+        self.canvas = customtkinter.CTkCanvas(self, cursor="cross", bg="black")
+        self.canvas.pack(fill=customtkinter.BOTH, expand=True)
+
+        self.startX = None
+        self.startY = None
+        self.rect = None
+
+        self.canvas.bind("<ButtonPress-1>", self.onClick)
+        self.canvas.bind("<B1-Motion>", self.onDrag)
+        self.canvas.bind("<ButtonRelease-1>", self.onRelease)
+
+    def onClick(self, event):
+        self.startX = self.canvas.canvasx(event.x)
+        self.startY = self.canvas.canvasy(event.y)
+        self.rect = self.canvas.create_rectangle(
+            self.startX,
+            self.startY,
+            self.startX,
+            self.startY,
+            outline="#538d4e",
+            width=3,
+        )
+
+    def onDrag(self, event):
+        curX, curY = (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
+        self.canvas.coords(self.rect, self.startX, self.startY, curX, curY)
+
+    def onRelease(self, event):
+        endX = self.canvas.canvasx(event.x)
+        endY = self.canvas.canvasy(event.y)
+
+        x1 = int(min(self.startX, endX))
+        y1 = int(min(self.startY, endY))
+        x2 = int(max(self.startX, endX))
+        y2 = int(max(self.startY, endY))
+
+        screenshot = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+
+        self.callback(screenshot)
+
+        self.destroy()
